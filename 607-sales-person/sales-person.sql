3003c45f22eb420a08e6c3dc13df9f1123b52bf5
Vinathi Kaithi <vinathireddy08@gmail.com>
2026-05-02 22:58:26 +0530

    Time: 1494 ms (28.98%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -4,7 +4,6 @@ FROM SalesPerson
 WHERE sales_id NOT IN (
     SELECT o.sales_id
     FROM Orders o
-    JOIN Company c
-        ON o.com_id = c.com_id
+    JOIN Company c ON o.com_id = c.com_id
     WHERE c.name = 'RED'
 );
